39ee8f8336bdce30a9791d99de518b3ad4a14cb6
manikanta9578 <manikanta9578@gmail.com>
2023-04-14 08:16:29 +0530

    Slider created

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://wallpapercave.com/wp/wp6488788.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://th.bing.com/th/id/OIP.isJ-gMKEmleI1VcfjeyUfgHaEc?pid=ImgDet&rs=1",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://th.bing.com/th/id/OIP.zorxw8MiuiPJ-Tr-srRcGAHaFx?pid=ImgDet&rs=1",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
